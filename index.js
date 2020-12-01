30b147fe3692166bf039a80441cc2365d3cd65fa
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2020-12-01 15:37:50 -0300

    Change r9x name to pamjs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { r9x } from './src/router.js'
+import { r9x as pamjs } from './src/router.js'
 import { observableFactory } from './src/observer.js'
 
-export { r9x, observableFactory }
+export { pamjs, observableFactory }
